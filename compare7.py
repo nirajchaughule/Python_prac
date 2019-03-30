4d8b933b283a6d28b2858bc0af2432adf0180ddf
nirajchaughule <48406063+nirajchaughule@users.noreply.github.com>
2019-03-30 18:18:33 +0530

    Add files via upload

---
+++ compare7.py
@@ -0,0 +1,58 @@
+print ("Enter the seven numbers to be compared")
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+e=int(input())
+f=int(input())
+g=int(input())
+
+if a>b:
+	if a>c:
+		if a>d:
+			if a>e:
+				if a>f:
+					if a>g:
+						print(f"{a} greatest")
+if b>a:
+	if b>c:
+		if b>d:
+			if b>e:
+				if b>f:
+					if b>g:
+						print(f"{b} greatest")
+if c>a:
+	if c>b:
+		if c>d:
+			if c>e:
+				if c>f:
+					if c>g:
+						print(f"{c} greatest")
+if d>a:
+	if d>b:
+		if d>c:
+			if d>e:
+				if d>f:
+					if d>g:
+						print(f"{d} greatest")
+if e>a:
+	if e>b:
+		if e>c:
+			if e>d:
+				if e>f:
+					if e>g:
+						print(f"{e} greatest")
+if f>a:
+	if f>b:
+		if f>c:
+			if f>d:
+				if f>e:
+					if f>g:
+						print(f"{e} greatest")
+if g>a:
+	if g>b:
+		if g>c:
+			if g>d:
+				if g>e:
+					if g>f:
+						print(f"{g} greatest")
