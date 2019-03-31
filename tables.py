097727f8be1351c64fb5f7618405ffdfbe60a8a4
nirajchaughule <48406063+nirajchaughule@users.noreply.github.com>
2019-03-31 16:31:47 +0530

    3 31 2019

---
+++ tables.py
@@ -0,0 +1,5 @@
+for i in range(2,16):
+	for j in range (1,11):
+		print(f"{i} X {j} = {i*j}");
+
+	print("\n")
---
+++ uinputstar.py
@@ -0,0 +1,3 @@
+a=int(input("Enter no of rows"));
+for i in range (1,a+1):
+	print ("*"*i)
