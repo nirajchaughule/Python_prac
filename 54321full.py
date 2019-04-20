a18b1e314c6912dcabb2ae9d45f1eec13aa02d6c
nirajchaughule <48406063+nirajchaughule@users.noreply.github.com>
2019-04-20 17:23:15 +0530

    half pattern

---
+++ 54321full.py
@@ -0,0 +1,17 @@
+for i in range(5,1,-1):
+	k=5
+	l=2
+	for j in range (1,i+1):
+			print(' ',end=f'{k}')
+			k=k-1
+			l=l+1
+	print(end=' \n')
+print(' 5')
+
+for i in range(1,5,):
+	k=5
+	for j in range (1,i+1):
+			print(' ',end=f'{k}')
+			k=k-1
+	print(end=' \n')
+print(' 5 4 3 2 1')
